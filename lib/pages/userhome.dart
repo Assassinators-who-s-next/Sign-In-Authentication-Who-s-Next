01d2dfe01bc44016bbb4e076ee5ff67aaf824830
Marlene Inoue <mminoue@ucsc.edu>
2023-04-20 14:14:44 -0700

    created basic drawer (will list games player is currently in)

--- lib/pages/userhome.dart
+++ lib/pages/userhome.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class UserHome extends StatelessWidget {
+class UserHome extends StatefulWidget {
+  @override
+  State<UserHome> createState() => _UserHomeState();
+}
+
+class _UserHomeState extends State<UserHome> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page', style: TextStyle(fontSize: 50)),
+    return Scaffold(
+      // top bar
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('game a'), // could have names of each game, or game code
+      ),
+      // a list of all games currently in, 3 lines on left
+      // TODO: each listTile will change the center and the appBar title
+      drawer: Drawer(
+        child: ListView(children: [
+          ListTile(
+            title: Text('data'),
+          )
+        ]),
+      ),
+      // TODO  target info
+      body: const Center(
+        child: Text('HOme page'),
+      ),
     );
   }
 }
