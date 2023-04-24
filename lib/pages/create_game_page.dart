5a725cddfc1a86fb4e5621c56cb2e05461c07019
Marlene Inoue <mminoue@ucsc.edu>
2023-04-24 11:34:10 -0700

    init create game page

---
+++ lib/pages/create_game_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CreateGamePage extends StatefulWidget {
+  @override
+  State<CreateGamePage> createState() => _CreateGamePage();
+}
+
+class _CreateGamePage extends State<CreateGamePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('hello');
+  }
+}
