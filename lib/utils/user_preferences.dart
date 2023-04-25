c8b05f9014e2a3b72b77a11e81031d28cd44cce0
harrypantsguy <harrypants2014@gmail.com>
2023-04-25 11:06:10 -0700

    Adjusted file path to placeholder image

--- lib/utils/user_preferences.dart
+++ lib/utils/user_preferences.dart
@@ -2,7 +2,7 @@ import 'package:basic_auth/models/user_data.dart';
 
 class UserPreferences {
   static const String placeholderImagePath =
-      "images/placeHolderProfileImage.jpg";
+      "lib/images/placeHolderProfileImage.jpg";
   static const user = UserData(
       imagePath: null,
       name: "Joe Smith",
