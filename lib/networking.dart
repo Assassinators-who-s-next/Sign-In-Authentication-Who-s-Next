4cfd51f9aaa3f9ec7c7a7d6cc37d47ad27d78632
Aaron B <dbrunckh@ucsc.edu>
2023-05-10 14:53:46 -0700

    Added way to set and load a user.

--- lib/networking.dart
+++ lib/networking.dart
@@ -9,10 +9,61 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'utils/random_string_generator.dart';
 import 'game_group.dart';
 import 'globals.dart' as globals;
+import 'models/match_options.dart';
+import 'models/user_data.dart';
 
 final storage = FirebaseStorage.instance;
 
 // sends
+
+Future<UserData> get_user_data(String user_id) async {
+  CollectionReference usersRef = FirebaseFirestore.instance.collection('users');
+
+  DocumentSnapshot userDocument = await usersRef.doc(user_id).get();
+
+  if (userDocument.exists) {
+    // Extract field values from the document snapshot
+    String? imagePath = userDocument.get('imagePath');
+    String name = userDocument.get('name');
+    String email = userDocument.get('email');
+    String pronouns = userDocument.get('pronouns');
+    String description = userDocument.get('description');
+    String frequentedLocations = userDocument.get('frequentedLocations');
+
+    // Create a UserData object with the extracted field values
+    UserData userData = UserData(
+      imagePath: imagePath,
+      name: name,
+      email: email,
+      pronouns: pronouns,
+      description: description,
+      frequentedLocations: frequentedLocations,
+    );
+
+    return userData;
+  } else {
+    throw Exception('Document does not exist');
+    // or
+    // return null;
+  }
+}
+
+void set_user_data(String userID, UserData userData) async {
+  CollectionReference usersRef = FirebaseFirestore.instance.collection('users');
+
+  await usersRef.doc(userID).set({
+    'user_id': userID,
+    'points': 0,
+    'name': userData.name,
+    'email': userData.email,
+    'pronouns': userData.pronouns,
+    'description': userData.description,
+    'frequentedLocations': userData.frequentedLocations
+  }
+
+      // Add any other fields you want to initialize here
+      );
+}
 
 void login_custom(BuildContext context, String userName, String password) {
   String my_name = "temp_user";
