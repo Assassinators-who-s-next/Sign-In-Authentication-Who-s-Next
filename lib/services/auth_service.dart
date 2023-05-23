341238dcb3a250f5748dba50fe37627c09caa8ea
Marlene Inoue <mminoue@ucsc.edu>
2023-05-23 13:42:44 -0700

    sign in and out works on mobile

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,6 +4,10 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import "package:basic_auth/globals.dart";
 
+// to check current platform
+// source: https://stackoverflow.com/questions/71249485/flutter-web-is-giving-error-about-unsupported-operation-platform-operatingsyst
+import 'package:flutter/foundation.dart';
+
 class AuthService {
   final GoogleSignIn googleSignIn = GoogleSignIn();
   String? name;
@@ -70,17 +74,48 @@ class AuthService {
     await Firebase.initializeApp();
     User? user;
 
-    // The `GoogleAuthProvider` can only be used while running on the web
-    GoogleAuthProvider authProvider = GoogleAuthProvider();
+    // do mobile signin if platform is mobile
+    if (defaultTargetPlatform == TargetPlatform.android ||
+        defaultTargetPlatform == TargetPlatform.iOS) {
+      print('mobile');
+      // begin interactive sign in
+      final GoogleSignInAccount? google_user = await GoogleSignIn().signIn();
 
-    try {
-      final UserCredential userCredential =
-          await _auth.signInWithPopup(authProvider);
+      // obtain auth details from request
+      final GoogleSignInAuthentication google_auth =
+          await google_user!.authentication;
 
-      user = userCredential.user;
-      fireBaseUser = user;
-    } catch (e) {
-      print(e);
+      // create new credential for user
+      final credential = GoogleAuthProvider.credential(
+          accessToken: google_auth.accessToken, idToken: google_auth.idToken);
+
+      try {
+        final UserCredential userCredential =
+            await _auth.signInWithCredential(credential);
+
+        user = userCredential.user;
+        print(userCredential.user);
+        fireBaseUser = user;
+      } catch (e) {
+        print(e);
+      }
+
+      // otherwise do the popup with web (not sure if works with MacOS or Windows)
+    } else {
+      print('not mobile');
+      // The `GoogleAuthProvider` can only be used while running on the web
+      GoogleAuthProvider authProvider = GoogleAuthProvider();
+
+      try {
+        final UserCredential userCredential =
+            await _auth.signInWithPopup(authProvider);
+
+        user = userCredential.user;
+        print(userCredential.user);
+        fireBaseUser = user;
+      } catch (e) {
+        print(e);
+      }
     }
 
     if (user != null) {
